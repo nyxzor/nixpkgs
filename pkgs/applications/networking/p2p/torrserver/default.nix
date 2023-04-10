b84444f7f78228eb496aaea375c7d8548d66e724
nyx <dennyx@inbox.ru>
2023-04-10 09:25:29 +0700

    Try add Yarn. No build.

--- pkgs/applications/networking/p2p/torrserver/default.nix
+++ pkgs/applications/networking/p2p/torrserver/default.nix
@@ -10,34 +10,47 @@
 , nixosTests
 }:
 
-buildGoModule rec {
+
+let
   pname = "torrserver";
   version = "121";
-
   src = fetchFromGitHub {
     rev = "MatriX.${version}";
     owner = "YouROK";
     repo = "TorrServer";
     sha256 = "sha256-xFUebXoGSqao7PDGNqk8jfkp64WHlJOBQtp7wsyw5Mc=";
   };
+  web = mkYarnPackage {
+    inherit pname version;
+    src = "${src}/web";
 
+#    yarnNix = ./yarn.nix;
+    yarnLock = ./yarn.lock;
+    yarnFlags = [
+      "--offline"
+    ];
+#    packageJSON = ./package.json;
+    buildPhase = ''
+      OUTPUT_DIR=$out yarn --offline build
+    '';
+  };
+in
+buildGoModule {
+  inherit pname version;
   vendorHash = null;
-
-#  nativeBuildInputs = [ yarn ];
-
+  src = "${web}/server";
   buildPhase = ''
-    go run ./gen_web.go
-    cd ./server
-    go build -o /dist/TorrServer $src/cmd
+#    go run gen_web.go
+    go build -o /dist/TorrServer ${src}/cmd
   '';
 
   ldflags = [
     "-s" "-w"
     ];
 
-  doCheck = true;
+  doCheck = false;
 
-  passthru.tests = { inherit (nixosTests) torrserver; };
+#  passthru.tests = { inherit (nixosTests) torrserver; };
 
   meta = with lib; {
     description = "Torrent stream server";
